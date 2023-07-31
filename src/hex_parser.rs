97f85e3e007d93b6abdb7987f501e2ec695e315a
bubba2k <ali4bay@gmail.com>
2023-07-31 23:08:29 +0200

    Add comments

--- src/hex_parser.rs
+++ src/hex_parser.rs
@@ -5,9 +5,12 @@ use std::vec::Vec;
 pub fn bytevec_from_hexfile(file_path: &String) -> Result<Vec<[u8; 3]>, &'static str> {
     let mut bytevec: Vec<[u8; 3]> = Vec::new();
 
+    // Split the file into words and attempt to parse them
     for word in fs::read_to_string(file_path).unwrap().split_whitespace() {
         let res = <[u8; 3]>::from_hex(word);
 
+        // If we come across a non-hex word, we simply display a warning
+        // and ignore it. Probably should figure out a better solution later.
         match res {
             Ok(val) => bytevec.push(val),
             Err(msg) => eprintln!("Warning while parsing hexfile: {}", msg),
