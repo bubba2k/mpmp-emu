f373c4a2e274f10038a8d578e6fb99b0066e6f0b
bubba2k <ali4bay@gmail.com>
2023-08-01 01:18:44 +0200

    Implement opcode enum, update tests

--- src/decoder.rs
+++ src/decoder.rs
@@ -5,6 +5,49 @@ pub struct InstructionWord {
                  // to do it
 }
 
+// Enumeration of all possible opcodes
+#[derive(FromPrimitive, Debug, PartialEq, Eq)]
+enum Opcode {
+    ADD = 0x0,
+    ADD3 = 0x1,
+    ADC = 0x2,
+    SUB = 0x3,
+    SUBC = 0x4,
+    INC = 0x5,
+    DEC = 0x6,
+    MUL = 0x7,
+    TST = 0x8,
+    AND = 0x9,
+    OR = 0xa,
+    NOT = 0xb,
+    NEG = 0xc,
+    XOR = 0xd,
+    XNOR = 0xe,
+    SHL = 0xf,
+    SHR = 0x10,
+
+    MOV = 0x48,
+
+    JMP = 0x50,
+    JZ = 0x51,
+    JNZ = 0x52,
+    JC = 0x53,
+    JNC = 0x54,
+    JRCON = 0x58,
+    JZR = 0x59,
+    JNZR = 0x5a,
+    JCR = 0x5b,
+    JNCR = 0x5c,
+
+    ST = 0x68,
+    LD = 0x69,
+
+    NOP = 0x6c,
+    DBG = 0x7e,
+    HLT = 0x7f,
+    LDC = 0x80,
+}
+
 impl From<u32> for InstructionWord {
     fn from(value: u32) -> Self {
         InstructionWord { buffer: value }
@@ -47,12 +90,21 @@ impl InstructionWord {
         Ok(res_val)
     }
 
-    fn get_opcode(&self) -> u32 {
+    fn get_opcode(&self) -> Opcode {
         // Since the lower 4 bits of 16 bit constants in LDC (load constant)
         // operations are stored in the lower 4 bits of the opcode,
-        // and all opcodes above 0x80 are also just LDC ops, we might
-        // as well clamp it here, because why not.
-        self.get_bits(0, 7).unwrap().clamp(0, 0x80)
+        // and all opcodes above 0x80 are also just LDC ops, we clamp it here.
+        let value = self.get_bits(0, 7).unwrap().clamp(0, 0x80);
+
+        // Attempt to parse an enum value from the u32
+        return match num::FromPrimitive::from_u32(value) {
+            Some(val) => val,
+            None => {
+                // Print a warning indicating unknown opcode and return NOP
+                eprintln!("Unknown opcode in {:#8x}", self.buffer);
+                Opcode::NOP
+            }
+        };
     }
 
     fn get_op_a(&self) -> u32 {
@@ -76,7 +128,6 @@ impl InstructionWord {
     }
 
     fn get_constant16(&self) -> u32 {
-        // TODO: Figure this out and how it might interfere with get_opcode
         let lower_4: u32 = self.get_bits(0, 3).unwrap();
         let upper_12: u32 = self.get_bits(8, 19).unwrap();
 
@@ -148,70 +199,70 @@ mod tests {
 
     #[test]
     fn instructionword_getter_test() {
-        let word2 = InstructionWord::from(INSTR2);
-        assert_eq!(word2.get_opcode(), 0x5);
-        assert_eq!(word2.get_target(), 0x0);
-        assert_eq!(word2.get_op_a(), 0x0);
-
         let word1 = InstructionWord::from(INSTR1);
-        assert_eq!(word1.get_opcode(), 0x80);
+        assert_eq!(word1.get_opcode(), Opcode::LDC);
         assert_eq!(word1.get_load_address(), 2);
         assert_eq!(word1.get_constant16(), 0x8001);
 
+        let word2 = InstructionWord::from(INSTR2);
+        assert_eq!(word2.get_opcode(), Opcode::INC);
+        assert_eq!(word2.get_target(), 0x0);
+        assert_eq!(word2.get_op_a(), 0x0);
+
         let word3 = InstructionWord::from(INSTR3);
-        assert_eq!(word3.get_opcode(), 0x0);
+        assert_eq!(word3.get_opcode(), Opcode::ADD);
         assert_eq!(word3.get_target(), 0x0);
         assert_eq!(word3.get_op_a(), 0x1);
         assert_eq!(word3.get_op_b(), 0x2);
 
         let word4 = InstructionWord::from(INSTR4);
-        assert_eq!(word4.get_opcode(), 0x1);
+        assert_eq!(word4.get_opcode(), Opcode::ADD3);
         assert_eq!(word4.get_target(), 0x3);
         assert_eq!(word4.get_op_a(), 0x4);
         assert_eq!(word4.get_op_b(), 0x5);
         assert_eq!(word4.get_op_c(), 0x2);
 
         let word5 = InstructionWord::from(INSTR5);
-        assert_eq!(word5.get_opcode(), 0x3);
+        assert_eq!(word5.get_opcode(), Opcode::SUB);
         assert_eq!(word5.get_target(), 0x0);
         assert_eq!(word5.get_op_a(), 0x4);
         assert_eq!(word5.get_op_b(), 0x5);
 
         let word6 = InstructionWord::from(INSTR6);
-        assert_eq!(word6.get_opcode(), 0x4);
+        assert_eq!(word6.get_opcode(), Opcode::SUBC);
         assert_eq!(word6.get_target(), 0x2);
         assert_eq!(word6.get_op_a(), 0x1);
         assert_eq!(word6.get_op_b(), 0x4);
 
         let word7 = InstructionWord::from(INSTR7);
-        assert_eq!(word7.get_opcode(), 0x80);
+        assert_eq!(word7.get_opcode(), Opcode::LDC);
         assert_eq!(word7.get_load_address(), 0x5);
         assert_eq!(word7.get_constant16(), 0x7832);
 
         let word8 = InstructionWord::from(INSTR8);
-        assert_eq!(word8.get_opcode(), 0xa);
+        assert_eq!(word8.get_opcode(), Opcode::OR);
         assert_eq!(word8.get_target(), 0x0);
         assert_eq!(word8.get_op_a(), 0x1);
         assert_eq!(word8.get_op_b(), 0x4);
 
         let word9 = InstructionWord::from(INSTR9);
-        assert_eq!(word9.get_opcode(), 0x6c);
+        assert_eq!(word9.get_opcode(), Opcode::NOP);
 
         let word10 = InstructionWord::from(INSTR10);
-        assert_eq!(word10.get_opcode(), 0x51);
+        assert_eq!(word10.get_opcode(), Opcode::JZ);
         assert_eq!(word10.get_op_a(), 0x2);
 
         let word11 = InstructionWord::from(INSTR11);
-        assert_eq!(word11.get_opcode(), 0x50);
+        assert_eq!(word11.get_opcode(), Opcode::JMP);
         assert_eq!(word11.get_op_a(), 0x3);
 
         let word12 = InstructionWord::from(INSTR12);
-        assert_eq!(word12.get_opcode(), 0x5B);
+        assert_eq!(word12.get_opcode(), Opcode::JCR);
         assert_eq!(word12.get_constant12(), 4);
         // The argument is 5, but actually we enoly jumo
         // 4 steps due to program counter incrementing anyway
 
         let wordh = InstructionWord::from(INSTRH);
-        assert_eq!(wordh.get_opcode(), 0x7f);
+        assert_eq!(wordh.get_opcode(), Opcode::HLT);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod decoder;
 mod hex_parser;
+mod ir;
+
+extern crate num;
+#[macro_use]
+extern crate num_derive;
 
 fn main() {
     println!("Hello, world!");
