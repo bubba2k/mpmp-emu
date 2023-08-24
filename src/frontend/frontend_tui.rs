d159848c74eeee58a10aabe48b6b8d9fcfdd3798
bubba2k <ali4bay@gmail.com>
2023-08-24 20:42:51 +0200

    Implement basic tui frontend prototype

---
+++ src/frontend/frontend_tui.rs
@@ -0,0 +1,5 @@
+
+
+struct App {
+    cpu: mut CpuState,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::error::Error;
+use std::io::{self, Stdout};
+use std::rc::Rc;
+use std::time::Duration;
+
+use ratatui::prelude::{
+    Alignment, Color, Constraint, CrosstermBackend, Direction, Layout, Rect, Style,
+};
+use ratatui::widgets::{Block, Borders, Cell, Padding, Paragraph, Row, Table, Wrap};
+use ratatui::Terminal;
+
+use crossterm::{
+    event::{self, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+use self::program::Program;
+use self::runtime::CpuState;
+
+mod frontend;
 mod decoder;
 mod hex_parser;
 mod ir;
@@ -8,30 +30,148 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
-use std::env;
-use std::process::exit;
+extern crate crossterm;
+extern crate ratatui;
 
-use crate::hex_parser::bytevec_from_hexfile;
-use crate::program::Program;
-use crate::runtime::CpuState;
-
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-
-    if args.len() < 2 {
-        println!("Usage: <hex_file_path>");
-        exit(1);
-    }
-
-    let hex_vec = bytevec_from_hexfile(&args[1]).unwrap();
-    let program = Program::from(hex_vec.as_slice());
-
+    let bytevec = hex_parser::bytevec_from_hexfile(&args[1]).unwrap();
+    let program = Program::from(bytevec.as_slice());
     let mut cpu = CpuState::default();
 
-    while !cpu.received_halt {
-        cpu.execute_next_prog_op(&program);
-    }
+    let mut terminal = setup_terminal()?;
+    run(&mut terminal, &mut cpu, &program)?;
+    restore_terminal(&mut terminal)?;
+    Ok(())
+}
 
-    println!("{}", cpu.ostream.string);
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
+    let mut stdout = io::stdout();
+    enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+    Ok(Terminal::new(CrosstermBackend::new(stdout))?)
+}
+
+fn restore_terminal(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+) -> Result<(), Box<dyn Error>> {
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen,)?;
+    Ok(terminal.show_cursor()?)
+}
+
+fn run(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    cpu: &mut CpuState,
+    program: &Program,
+) -> Result<(), Box<dyn Error>> {
+    Ok(loop {
+        // Drawing
+        terminal.draw(|frame| {
+            let toplevel_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                        Constraint::Percentage(33),
+                        Constraint::Percentage(33),
+                        Constraint::Percentage(33),
+                    ]
+                    .as_ref(),
+                )
+                .split(frame.size());
+
+            let tty_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(80), Constraint::Percentage(20)].as_ref())
+                .split(toplevel_chunks[1]);
+
+            let cpustate_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+                .split(toplevel_chunks[0]);
+
+            let stdout_block = Block::default()
+                .title(" Output ")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .padding(Padding::new(1, 1, 1, 1));
+            let keyboard_buf_block = Block::default()
+                .title(" Keyboard Buffer ")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL);
+            let register_block = Block::default()
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL);
+            let ram_block = Block::default()
+                .title("RAM")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .padding(Padding::new(1, 1, 1, 1));
+            let pmem_block = Block::default()
+                .title(" Program Memory ")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .padding(Padding::new(1, 1, 1, 1));
+
+            let stdout_pgraph = Paragraph::new(cpu.ostream.string.clone())
+                .block(stdout_block)
+                .wrap(Wrap { trim: false });
+            let pmem_pgraph = Paragraph::new("Program memory")
+                .block(pmem_block)
+                .wrap(Wrap { trim: true });
+            let register_table = Table::new(vec![
+                Row::new(vec![
+                    Cell::from("%reg0"),
+                    Cell::from(format!("{:#4X}", cpu.registers[0])),
+                ]),
+                Row::new(vec![
+                    Cell::from("%reg1"),
+                    Cell::from(format!("{:#4X}", cpu.registers[1])),
+                ]),
+                Row::new(vec![
+                    Cell::from("%reg2"),
+                    Cell::from(format!("{:#4X}", cpu.registers[2])),
+                ]),
+                Row::new(vec![
+                    Cell::from("%reg3"),
+                    Cell::from(format!("{:#4X}", cpu.registers[3])),
+                ]),
+                Row::new(vec![
+                    Cell::from("%reg4"),
+                    Cell::from(format!("{:#4X}", cpu.registers[4])),
+                ]),
+                Row::new(vec![
+                    Cell::from("%reg5"),
+                    Cell::from(format!("{:#4X}", cpu.registers[5])),
+                ]),
+                Row::new(vec![
+                    Cell::from("PC"),
+                    Cell::from(format!("{:#4X}", cpu.pcounter)),
+                ]),
+            ])
+            .block(register_block)
+            .column_spacing(1)
+            .widths([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref());
+
+            frame.render_widget(keyboard_buf_block, tty_chunks[1]);
+            frame.render_widget(stdout_pgraph, tty_chunks[0]);
+            frame.render_widget(register_table, cpustate_chunks[0]);
+            frame.render_widget(ram_block, cpustate_chunks[1]);
+            frame.render_widget(pmem_pgraph, toplevel_chunks[2]);
+        })?;
+
+        // CPU update
+        if !cpu.received_halt {
+            cpu.execute_next_prog_op(program);
+        }
+
+        // Event handling
+        if event::poll(Duration::from_millis(50))? {
+            if let crossterm::event::Event::Key(key) = event::read()? {
+                if KeyCode::Char('q') == key.code {
+                    break;
+                }
+            }
+        }
+    })
 }
